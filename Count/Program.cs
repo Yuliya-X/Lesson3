3043cd826b6b41a36dd6a92b587b4510587fc3ec
Yuliya <darkwader@mail.ru>
2023-02-22 19:30:53 +0300

    задачи семинара 3

---
+++ Count/Program.cs
@@ -0,0 +1,32 @@
+﻿//дан массив: наполнени числами от 1 до N
+//Задача:выяснить сколько раз какое число встречается
+
+// задан массив +
+int[] numbers = { 1, 3, 1, 2, 1, 2, 4, 5, 1, 2, 5, 4 }; 
+int length = numbers.Length;
+
+
+// ввод данных пользователем +
+int Input(string number)
+{
+    Console.Write(number + " ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// вывод количества раз +
+void Find(int[] numbers)
+{
+    int index = 0;
+    int count = 0;
+    int number = Input("Введите число: ");
+
+    for (index = 0; index < numbers.Length; index++)
+        {
+            if (number == numbers[index])
+            count = count + 1;
+        }
+        
+        Console.WriteLine(count + " повтора");
+            
+    }
+Find(numbers);
---
+++ Mix/Program.cs
@@ -0,0 +1,68 @@
+﻿// создать массив
+
+int[] CreateArray(int size)
+{
+    return new int[size];
+}
+
+
+// Ввод данных
+int Input(string text)
+{
+    Console.Write(text + " ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Заполнение руками
+  void FillByUser(int[] array)
+  {
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+      array[index] = Input("Введите array[" + index + "]: ");
+      index++;
+    }
+  }
+
+  //Mix
+void Mix(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        int i = index;
+        int j = new Random().Next(i + 1);
+        index++;
+        {
+            int tmp = array[j];
+            array[j] = array[i];
+            array[i] = tmp;
+        }
+    }
+}
+
+// Печать
+  void Print1(int[] array)
+  {
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+      Console.Write(array[index] + " ");
+      index++;
+    }
+    System.Console.WriteLine();
+  }
+
+
+
+int count = Input("Введите размер массива ");
+int[] numbers = CreateArray(count);
+Print1(numbers);
+FillByUser(numbers);
+Print1(numbers);
+Mix(numbers);
+Print1(numbers);
+
